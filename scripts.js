92ea6da675a1543fc766cdadfee8e3e480ec5446
Rodrigo Cevas <rcevas@gmail.com>
2019-10-18 13:44:24 +0200

    FASE 3 Y 4

--- scripts.js
+++ scripts.js
@@ -32,8 +32,42 @@ for (let i of nombre) {
     
 }
 
-
+ 
 //FASE 3
+
+var nombre = ['R','O','D','R','I','G','O'];
+
+var  count = {};
+nombre.forEach(function(i) { count[i] = (count[i]||0) + 1;});
+console.log(count);
+
+//or
+
+var letras = {};
+nombre.map(function (nom) {
+    return letras[nom] = (letras[nom]||0) + 1;
+} ) 
+console.log(letras);
+
+
+//FASE 4
+
+
+
+
+let name = ['R','O','D','R','I','G','O'];
+let surname = ['C','E','V','A','S'];
+
+name.push(" ");
+
+let fullname = name.concat(surname);
+
+console.log(fullname);
+
+
+
+
+
 
 
 
